caff829083ea26ef74e8b0043a265e373d44895e
UncleH <ecomerim1@gmail.com>
2021-10-16 02:02:37 +0300

    Full Video Screen

--- app/src/main/java/com/halit/myvideo/Player.java
+++ app/src/main/java/com/halit/myvideo/Player.java
@@ -1,11 +1,18 @@
 package com.halit.myvideo;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -13,18 +20,25 @@ import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Player extends AppCompatActivity {
 
     public static final String TAG = "TAG";
     ProgressBar spliner;
+    ImageView fullScreenOp;
+    FrameLayout frameLayout;
+    VideoView videoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         spliner = findViewById(R.id.progressBar);
+        fullScreenOp = findViewById(R.id.fullScreenOp);
+        frameLayout = findViewById(R.id.frameLayout);
 
         Intent i = getIntent();
         Bundle data = i.getExtras();
@@ -36,7 +50,7 @@ public class Player extends AppCompatActivity {
 
         TextView title = findViewById(R.id.videoTitle);
         TextView desc = findViewById(R.id.videoDesc);
-        final VideoView videoPlayer = findViewById(R.id.videoView);
+        videoPlayer = findViewById(R.id.videoView);
 
         title.setText(v.getTitle());
         desc.setText(v.getDescription());
@@ -54,6 +68,18 @@ public class Player extends AppCompatActivity {
             }
         });
 
+        fullScreenOp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fullScreenOp.setVisibility(View.GONE);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                getSupportActionBar().hide();
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                frameLayout.setLayoutParams(new ConstraintLayout.LayoutParams(new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)));
+                videoPlayer.setLayoutParams(new FrameLayout.LayoutParams(new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)));
+            }
+        });
+
     }
 
     @Override
@@ -62,5 +88,23 @@ public class Player extends AppCompatActivity {
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        fullScreenOp.setVisibility(View.VISIBLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getSupportActionBar().show();
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        int heightValue = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,220,getResources().getDisplayMetrics());
+        frameLayout.setLayoutParams(new ConstraintLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,heightValue)));
+        videoPlayer.setLayoutParams(new FrameLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,heightValue)));
+        int orientation = getResources().getConfiguration().orientation;
+
+        if(orientation == Configuration.ORIENTATION_PORTRAIT){
+            super.onBackPressed();
+        }
+
+
     }
 }
